9677526321543baf9a61322211e1582d4775372c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-01 17:15:21 +0300

    db migrations

---
+++ database/migrations/2023_08_01_165058_create_passwords_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->id();
+            $table->string('resource');
+            $table->string('login');
+            $table->string('password');
+            $table->text('additional_info');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('passwords');
+    }
+};
