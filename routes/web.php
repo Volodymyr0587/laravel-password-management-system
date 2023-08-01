18cd063b2a639902233c726a9d7e4fd994edb747
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-01 17:42:37 +0300

    start

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index', ['message' => 'Hello. Keep doing the project! :)']);
-});
+    return view('home');
+})->name('home');
 
 
