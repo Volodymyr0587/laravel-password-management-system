9d0072e2c81e37939b1c3f965c343e8d3906b6d7
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-02 21:36:52 +0300

    mt-4

--- resources/views/passwords/index.blade.php
+++ resources/views/passwords/index.blade.php
@@ -9,7 +9,7 @@
                     Create
                 </a>
 
-                <div class="mt-2">Total records: {{ auth()->user()->passwords->count() }}</div>
+                <div class="mt-4">Total records: {{ auth()->user()->passwords->count() }}</div>
             </div>
             <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
