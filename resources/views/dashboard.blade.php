588026d23d42163acc9099f8523e72fe82e24278
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-08-02 18:10:04 +0300

    Dashboard table view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,23 @@
 
 
             <div class="mt-4">
-                Hello {{ auth()->user()->username }}. You are store <b>{{ auth()->user()->passwords->where('created_at', '>=', now()->subDays(7))->count() }}</b> paswords at last week.
+                Hello {{ auth()->user()->username }}. You are store
+                <b>{{ auth()->user()->passwords->where('created_at', '>=', now()->subDays(7))->count() }}</b> paswords at
+                last week.
                 <p class="mt-4">Passwords were created for: </p>
-                @foreach (auth()->user()->passwords->pluck('resource')->unique() as $resource)
-                    <p class="m-4">{{ $resource }}</p>
-                @endforeach
+
+                <table class="m-4">
+                    <tbody>
+                        @foreach (auth()->user()->passwords as $password)
+                            <tr>
+                                <td>{{ $password->resource }}</td>
+                                <td class="text-gray-600 text-sm px-10">Created {{ $password->created_at->diffForHumans() }}
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
